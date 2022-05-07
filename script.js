1110aacc3cc9bd13657faed99cb4cc239b04eaa1
shweta604 <shwetapatil604@gmail.com>
2022-05-08 02:42:10 +0530

    create input box to set an alarm

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 const clock = document.getElementById('current-time');
+const hour = document.getElementById('hour');
+const minute = document.getElementById('minute');
+const second = document.getElementById('second');
 
 // displays current time of the day
 function currentTime() {
@@ -10,3 +13,31 @@ function currentTime() {
 
 // to repeat it after every one second
 setInterval(currentTime, 1000);
+
+// add 0 to 12 range for hour drop down
+function displayHour(time){
+    let select = time; // for selecting the second and minute option
+    let endPoint = 12;
+
+    for(let i = 0; i <= endPoint; i++){
+        // to create an option dynamically and add it to a select box
+        select.options[select.options.length] = new Option(i < 10 ? '0' + i : i);
+    }
+}
+
+displayHour(hour);
+
+// add 0 to 59 range to minute and second drop down
+function displayMinandSec(time){
+    let select = time; // for selecting the second and minute option
+    let endPoint = 59;
+
+    for(let i = 0; i <= endPoint; i++){
+        // to create an option dynamically and add it to a select box
+        select.options[select.options.length] = new Option(i < 10 ? '0' + i : i);
+    }
+}
+
+displayMinandSec(minute); 
+displayMinandSec(second);
+
