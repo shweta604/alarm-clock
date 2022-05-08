ab64e3a6aa56ee3848341b8368ed706107bcc300
shweta604 <shwetapatil604@gmail.com>
2022-05-08 23:24:45 +0530

    create set alarm function and display alert when the alarm goes off

--- script.js
+++ script.js
@@ -2,13 +2,29 @@ const clock = document.getElementById('current-time');
 const hour = document.getElementById('hour');
 const minute = document.getElementById('minute');
 const second = document.getElementById('second');
+const setAlarm = document.getElementById('on');
+const clearAlarm = document.getElementById('off');
+var alarmList = document.getElementById('alarm-list');
+const ampm = document.getElementById('am-pm');
+
+// contains all the set alarms
+var alarms = [];
+
+//current time
+var now;
 
 // displays current time of the day
 function currentTime() {
     let today = new Date(); // creating a date object with current date and time. Date objects are static. The computer time is ticking, but date objects are not.
-    let currentTime = today.toLocaleTimeString(); // it is going to take the current time of the pc(local time).
+    now = today.toLocaleTimeString(); // it is going to take the current time of the pc(local time).
 
-    clock.innerHTML = currentTime;
+    // calling ringing function when now is equal to one of the element in the alarms array
+    if(alarms.includes(now)){
+        console.log('ringing...');
+        ringing(now);
+    }
+
+    clock.innerHTML = now;
 }
 
 // to repeat it after every one second
@@ -16,28 +32,67 @@ setInterval(currentTime, 1000);
 
 // add 0 to 12 range for hour drop down
 function displayHour(time){
-    let select = time; // for selecting the second and minute option
+    let select = time; // for selecting the hour option
     let endPoint = 12;
 
-    for(let i = 0; i <= endPoint; i++){
-        // to create an option dynamically and add it to a select box
-        select.options[select.options.length] = new Option(i < 10 ? '0' + i : i);
+    for(let digit = 0; digit <= endPoint; digit++){
+        // to create an option dynamically
+        select.options[select.options.length] = new Option(digit < 10 ? '0' + digit : digit);
     }
 }
 
 displayHour(hour);
 
-// add 0 to 59 range to minute and second drop down
+// add 0 to 59 range for minute drop down
 function displayMinandSec(time){
-    let select = time; // for selecting the second and minute option
+    let select = time; // for selecting the minute and second option
     let endPoint = 59;
 
-    for(let i = 0; i <= endPoint; i++){
-        // to create an option dynamically and add it to a select box
-        select.options[select.options.length] = new Option(i < 10 ? '0' + i : i);
+    for(let digit = 0; digit <= endPoint; digit++){
+        // to create an option dynamically
+        select.options[select.options.length] = new Option(digit < 10 ? '0' + digit : digit);
     }
 }
 
-displayMinandSec(minute); 
+displayMinandSec(minute);
 displayMinandSec(second);
 
+// set an alarm for the specified time and push it into the ul 
+setAlarm.addEventListener('click', function(){
+    // time for the set alarm
+    let alarmValue = `${hour.value}:${minute.value}:${second.value} ${ampm.value}`;
+    // console.log(alarmValue);
+
+    // didn't set an alarm/ invalid time
+    if(hour.value == 0 && minute.value == 0 && second.value == 0){
+        window.alert('Please set an alarm');
+    }
+    // cannot set duplicate alarm
+    else if(alarms.includes(alarmValue)){
+        window.alert('Alarm is already set for the specified time');
+    }
+    else{
+        // push the value into alarm list 
+        alarms.push(alarmValue);
+
+        let newLi = document.createElement('li');
+
+        // every list item in the ul will look like this
+        let newLiInnerHtml = `
+            <span>${alarmValue}</span>
+            <button id="delete" class="btn btn-danger delete">Delete</button>`;
+
+        newLi.innerHTML = newLiInnerHtml;
+        // console.log(newLi);
+
+        alarmList.append(newLi);
+
+        // empty the alarmValue element 
+        alarmValue.value = '';
+    }
+});
+
+// show alert when the alarm goes off
+function ringing(now){
+    window.alert(`It's ${now}. click ok to turn off the alarm`);
+}
